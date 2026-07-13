9cb38d6efd00796aa9417172b0ec54d0be20bf18
thanwa2345 <thanwakitsakorn.com>
2026-07-13 19:59:04 +0700

    deploy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { photoRouter } = require('./modules/photo/photo.router');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
